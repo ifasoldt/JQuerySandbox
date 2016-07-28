b0096d3f60577e939fd9c01f5dba7e48fd6be584
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-07-27 22:48:06 -0400

    started modals

--- timeline.js
+++ timeline.js
@@ -16,6 +16,11 @@ function tweetBuilder(tweet){
   context = {picture: tweet.user.picture_url, body: tweet.body, name: tweet.user.name, time: moment(tweet.created_at, "YYYYMMDD").fromNow()}
   html = template(context)
   $('.place').append(html)
+  $.getJSON("https://guarded-bayou-26088.herokuapp.com/user/", {id: tweet.user.id}, function(data)
+{
+  console.log(data)
+
+})
 }
 
 // function tweetPic(picture_url)
